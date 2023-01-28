2533acf43fd7e40c5d61225306920d0776c3f06f
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2023-01-28 11:06:26 +0200

    Added burger menu

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,22 +1,12 @@
-(() => {
-    const refs = {
-      openMenuBtn: document.querySelector('[data-menu-open]'),
-      closeMenuBtn: document.querySelector('[data-menu-close]'),
-      menu: document.querySelector('[data-menu]'),
-      body: document.querySelector('body'),
-      menuList: document.querySelector('.mobile-list'),
-    };
-  
-    refs.openMenuBtn.addEventListener('click', toggleMenu);
-    refs.closeMenuBtn.addEventListener('click', toggleMenu);
-    refs.menuList.addEventListener('click', removeMenu);
-  
-    function toggleMenu() {
-      refs.menu.classList.toggle('is-hidden');
-      refs.body.classList.toggle('no-scroll');
-    }
-    function removeMenu() {
-      refs.menu.classList.add('is-hidden');
-      refs.body.classList.remove('no-scroll');
-    }
-  })();
+const openMenuBtn = document.querySelector('[data-menu-open]');
+const closeMenuBtn = document.querySelector('[data-menu-close]');
+const menu = document.querySelector('[data-menu]');
+const body = document.querySelector('body');
+
+openMenuBtn.addEventListener('click', toggleMenu);
+closeMenuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  menu.classList.toggle('is-hidden');
+  body.classList.toggle('no-scroll');
+}
