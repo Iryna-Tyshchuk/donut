4ae9f6fd4058397f01beadd30fc9b5dcfad1887b
Iryna-Tyshchuk <113116602+Iryna-Tyshchuk@users.noreply.github.com>
2023-01-30 10:03:27 +0000

    Deploying to gh-pages from @ Iryna-Tyshchuk/donut@6aff1cb0e05d7a3c40e23903bc856e711baf3e4a 🚀

---
+++ index.255ba510.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector(".view__more"),d=document.querySelector(".view__less"),t=document.querySelector(".text__more");e.addEventListener("click",(function(d){t.classList.remove("hidden"),e.classList.add("hidden")})),d.addEventListener("click",(function(d){t.classList.add("hidden"),e.classList.remove("hidden")}))})();
+//# sourceMappingURL=index.255ba510.js.map
